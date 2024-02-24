64576ec50403fcce0343af9e006ed9bb8c5d8877
MAO <mjose.araya@outlook.com>
2024-02-24 18:57:57 -0300

    cambios_server

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ import {logger} from 'logger-express'
 import cancionesRoutes from './routes/cancionesRoutes.js'
 import path from 'path'
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5500;
+
+// Obtener la ruta completa del archivo actual
+const __filename = new URL(import.meta.url).pathname;
+const _dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -12,7 +16,7 @@ app.use(express.json());
 app.use(cors());
 app.use(logger())
 
-app.use('/canciones', express.static(path.join(__dirname)))
+app.use('/canciones', express.static(path.join(_dirname, 'public')));
 
 app.use('/api/v1', cancionesRoutes)
 
